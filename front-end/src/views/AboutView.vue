2660c5ac8ec1126b01073d69c2331a25d5cc497c
Javier <sjavier11x@gmail.com>
2023-11-14 00:19:46 -0600

    forms-changes

--- front-end/src/views/AboutView.vue
+++ front-end/src/views/AboutView.vue
@@ -1,57 +1,102 @@
 <template>
   <div>
-    <h2>Crear formularios</h2>
     <form v-if="!currentForm && !isCreatingForm" @submit.prevent="createForm">
       <label>Nombre del Formulario:</label>
       <input v-model="formName" required />
-      <button type="submit">Crear Formulario</button>
+      <button class="button" type="submit">Crear Formulario</button>
     </form>
 
     <div v-if="currentForm">
       <h3>Formulario Actual: {{ currentForm }}</h3>
-
-      <form @submit.prevent="addOrUpdateQuestion">
+      <label for="list">Selecciona un item:</label>
+      <select id="list" v-model="selectedItem">
+        <option v-for="item in list" :key="item">{{ item }}</option>
+      </select>
+      <p>Has seleccionado: {{ selectedItem }}</p>
+      <!-- Cadena de texto/ numeros -->
+      <form v-if="selectedItem == 'Cadena Texto'" @submit.prevent="addOrUpdateQuestion">
         <label>Pregunta:</label>
         <div class="question-container" v-for="(question, index) in questions" :key="index">
           <input v-model="question.question" @blur="finishEditing(index)" @keyup.enter="finishEditing(index)" />
-          <button type="button" @click="editQuestion(index)">Editar</button>
         </div>
         <div class="question-container">
           <input v-model="newQuestion.question" required />
           <button type="submit">Agregar Pregunta</button>
         </div>
       </form>
-      <button @click="saveForm" v-if="questions.length > 0">Guardar Formulario</button>
+      <!-- Seleccio Unica -->
+      <form v-if="selectedItem == 'Valor numerico'">
+        <label>Pregunta:</label>
+        <div class="question-container" v-for="(question, index) in questions" :key="index">
+          <input v-model="question.question" @blur="finishEditing(index)" @keyup.enter="finishEditing(index)" />
+        </div>
+        <div class="question-container">
+          <input v-model="newQuestion.question" required />
+          <button type="submit">Agregar Pregunta</button>
+        </div>
+      </form>
+      <form v-if="selectedItem === 'Comboboxes'" @submit.prevent="addOrUpdateCombobox">
+        <div>
+          <h2>Comboboxes</h2>
+        </div>
+        <div class="question-container" v-for="(question, index) in questions" :key="index">
+          <label>Pregunta:</label>
+          <input v-model="question.question" @blur="finishEditing(index)" @keyup.enter="finishEditing(index)" />
+          <label>Respuestas (separadas por comas):</label>
+          <input v-model="question.answers" required />
+        </div>
+        <div class="question-container">
+          <label>Pregunta:</label>
+          <input v-model="newQuestion.question" required />
+          <label>Respuestas (separadas por comas):</label>
+          <input v-model="newQuestion.answers" required />
+          <button type="submit">Agregar Pregunta</button>
+        </div>
+      </form>
+      <form v-if="selectedItem == 'Tablas Editables'">
+        <div>
+          <h2>Tablas Editables</h2>
+        </div>
+      </form>
+      <button @click="saveForm">Guardar Formulario</button>
 
     </div>
-    <!-- Mostrar la lista de formularios guardados -->
     <div v-if="savedForms.length > 0 && !isCreatingForm">
       <h2>Formularios Guardados:</h2>
       <ul>
         <li v-for="(form, index) in savedForms" :key="index">
           {{ form.name }}
-          <button @click="generateLink">Generar Enlace</button>
+          <button class="button" @click="generateLink">Generar Enlace</button>
         </li>
-        
+
       </ul>
       <!-- <button @click="printFormsToConsole">Imprimir Formularios en Consola</button> -->
     </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
       formName: "",
       currentForm: null,
+      mostrarMenu: false,
+      list: ['Cadena Texto', 'Comboboxes', 'Registro Documentos', 'Tablas Editables'],
+      selectedItem: '',
       newQuestion: {
-        question: ""
+        question: "",
+        answers: ""
       },
       questions: [],
       savedForms: [],
       editingIndex: null,
-      isCreatingForm: false 
+      isCreatingForm: false,
+      questionTypes: {
+        'Cadena Texto': [],
+        'Valor numerico': [],
+        'Comboboxes': [],
+        'Tablas Editables': [],
+      },
     };
   },
   methods: {
@@ -61,24 +106,41 @@ export default {
       this.isCreatingForm = true;
     },
     addOrUpdateQuestion() {
+      const questionData = { question: this.newQuestion.question };
+
       if (this.editingIndex === null) {
-        // Agregar nueva pregunta
-        this.questions.push({ question: this.newQuestion.question });
+        this.questionTypes[this.selectedItem].push(questionData);
       } else {
-        // Actualizar pregunta existente
-        this.$set(this.questions, this.editingIndex, { question: this.newQuestion.question });
+        this.$set(this.questionTypes[this.selectedItem], this.editingIndex, questionData);
         this.editingIndex = null;
       }
+
       this.newQuestion.question = "";
     },
     saveForm() {
-      this.savedForms.push({
+      const formToSave = {
         name: this.currentForm,
-        questions: [...this.questions]
-      });
+        questions: [],
+      };
+      for (const type in this.questionTypes) {
+        formToSave.questions = formToSave.questions.concat(
+          this.questionTypes[type].map(question => ({
+            type: type,
+            question: question.question,
+            answers: question.answers || [],
+          }))
+        );
+      }
+      this.savedForms.push(formToSave);
       this.currentForm = null;
-      this.questions = [];
-      this.isCreatingForm = false; 
+      this.questionTypes = {
+        'Cadena Texto': [],
+        'Valor numerico': [],
+        'Comboboxes': [],
+        'Tablas Editables': [],
+      };
+      this.isCreatingForm = false;
+      this.selectedItem = '';
     },
     printFormsToConsole() {
       console.log("Formularios Guardados:", this.savedForms);
@@ -88,9 +150,52 @@ export default {
         this.editingIndex = null;
       }
     },
-    generateLink(){
-      //
-    }
+    generateLink() {
+      const formToSave = {
+        name: this.currentForm,
+        questions: [],
+      };
+
+      for (const type in this.questionTypes) {
+        formToSave.questions = formToSave.questions.concat(
+          this.questionTypes[type].map(question => ({
+            type: type,
+            question: question.question,
+            answers: question.answers || [],
+          }))
+        );
+      }
+      fetch('https://script.google.com/macros/s/AKfycbxB1DwCwGP3M_brntKBZURQdmQRNpxLC8j4Auass-soDMHBaXqlxedi7HdlA014G4m1/exec', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `formName=${encodeURIComponent(this.currentForm)}&questions=${encodeURIComponent(JSON.stringify(formToSave.questions))}`,
+      })
+        .then(response => response.text())
+        .then(formUrl => {
+          console.log('Formulario creado:', formUrl);
+        })
+        .catch(error => {
+          console.error('Error al crear el formulario:', error);
+        });
+    },
+    addOrUpdateCombobox() {
+      const questionData = {
+        question: this.newQuestion.question,
+        answers: this.newQuestion.answers.split(',').map(answer => answer.trim()),
+      };
+
+      if (this.editingIndex === null) {
+        this.questionTypes[this.selectedItem].push(questionData);
+      } else {
+        this.$set(this.questionTypes[this.selectedItem], this.editingIndex, questionData);
+        this.editingIndex = null;
+      }
+
+      this.newQuestion.question = "";
+      this.newQuestion.answers = "";
+    },
   }
 };
 </script>
@@ -105,10 +210,16 @@ form {
   flex: 1 1 0%;
   padding: 8rem 1rem 1rem;
 }
+
 h2 {
   font-size: 2rem;
   text-transform: uppercase;
   margin-bottom: 2rem;
+}
+
+h3 {
+  color: white;
+  background-color: rgb(48, 38, 121);
 }
 
 .question-container {
@@ -137,8 +248,17 @@ input:focus {
   opacity: 1;
 }
 
-button {
-  background-color: rgb(66, 108, 245);
+.question-container {
+  margin-bottom: 10px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.button {
+  background-color: rgb(48, 38, 121);
   color: #fff;
   font-weight: 700;
   padding: 0.5rem 1rem;
@@ -151,8 +271,9 @@ button {
 button:hover {
   background-color: #3457b4;
 }
+
 form.login input[type="submit"] {
-  background-color: rgb(66, 108, 245);
+  background-color: rgb(48, 38, 121);
   color: #fff;
   font-weight: 700;
   padding: 1rem 2rem;
